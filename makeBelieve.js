4549e7f0b8906a4408f7fd86f3ce38ac386f7e0c
Haukur Guðmundsson <haukur16@ru.is>
2019-01-30 22:31:28 +0000

    parent, grandparent

--- makeBelieve.js
+++ makeBelieve.js
@@ -21,6 +21,46 @@
         return tagNames;
     }
 
+    MakeBelieveElement.prototype.parent = function(selector) {
+ 
+        var parents = [];
+
+            if (selector) {
+              for (i = 0; i < this.nodes.length; i++) {
+                var parentNode = this.nodes[i].parentNode;
+                if(parentNode.matches(selector)) {
+                  parents.push(this.nodes[i].parentNode);
+                }
+              } 
+            }
+            else
+            for (var i = 0; i < this.nodes.length; i++) {
+                var currentElement = this.nodes[i];
+                parents.push(currentElement.parentNode.nodeName.toLowerCase());
+                }
+            return parents;
+
+};
+
+MakeBelieveElement.prototype.grandparent = function(selector) {
+    var grandParents = [];
+
+        if (selector) {
+          for (i = 0; i < this.nodes.length; i++) {
+            var grandParentNode = this.nodes[i].parentNode.parentNode;
+            if(grandParentNode.matches(selector)) {
+                grandParents.push(this.nodes[i].parentNode.parentNode);
+            }
+          } 
+        }
+        else
+        for (var i = 0; i < this.nodes.length; i++) {
+            var currentElement = this.nodes[i];
+            grandParents.push(currentElement.parentNode.parentNode.nodeName.toLowerCase());
+            }
+        return grandParents;
+
+};
     function query(cssSelector) {
         return new MakeBelieveElement(document.querySelectorAll(cssSelector));
     }
@@ -30,7 +70,18 @@
 
 var paragraphs = __('p');
 var divs = __('.item');
+var inputs = __('#my-form input');
+var parent = __('#password').parent();
+var formParent = __('#password').parent('form');
+var grandparent = __('#password').grandparent('div');
+var formgrandParent = __('#username').grandparent('body');
+
 console.log(paragraphs.getLength());
 console.log(paragraphs.getTagNames());
 console.log(divs.getLength());
 console.log(divs.getTagNames());
+console.log(inputs);
+console.log(parent);
+console.log(formParent);
+console.log(grandparent);
+console.log(formgrandParent);
